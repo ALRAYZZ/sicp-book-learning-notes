ec1f531de06f72a9a5b1f7cd513c09897794e9dc
Alejandro Montaner Roig <alroigcode@gmail.com>
2026-02-03 19:20:15 +0100

    add: create naming_enviroment section

---
+++ naming_enviroment.rkt
@@ -0,0 +1,22 @@
+#|A critical aspect of a programming language is the means it provides for using names to refer 
+to computational objects. We say that the name identifies a variable whose value is the object. |#
+
+#lang sicp
+
+(define size 2)
+
+size
+
+(* 5 size)
+
+
+(define pi 3.14)
+(define radius 10)
+
+(* pi (* radius radius))
+
+
+#|In general, computational objects may have very complex structures, and it would be extremely 
+inconvenient to have to remember and repeat their details each time we want to use them. 
+Indeed, complex programs are constructed by building, step by step,
+computational objects of increasing complexity.|#
